4302e25c4a72ce3556f2e404261b6276a632bfcd
Tazim <tazimulsohag@gmail.com>
2024-01-04 18:20:55 +0600

    base design ready

--- app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
+++ app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
@@ -1,14 +1,31 @@
 package co.banglabs.currencyconverter;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Spinner spnFrom, spnTo;
+    EditText editTextFrom, editTextTo;
+    TextView conversation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spnFrom = findViewById(R.id.spinnerFrom);
+        spnTo = findViewById(R.id.spinnerto);
+
+        editTextFrom = findViewById(R.id.edittxtfrom);
+        editTextTo = findViewById(R.id.edittxtto);
+
+        conversation = findViewById(R.id.conversationRate);
+
+
     }
 }
