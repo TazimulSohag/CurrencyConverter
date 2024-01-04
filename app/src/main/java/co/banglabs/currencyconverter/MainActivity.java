27c3539f2ad5e8fe5c99080f4739e77317ccbce7
Tazim <tazimulsohag@gmail.com>
2024-01-04 19:23:26 +0600

    today's season done

--- app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
+++ app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
@@ -3,8 +3,15 @@ package co.banglabs.currencyconverter;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -14,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     Spinner spnFrom, spnTo;
     EditText editTextFrom, editTextTo;
     TextView conversation;
+    Dialog from_dialog, to_dialog;
     ArrayList<String> arraylist;
     String convert_from_value, convert_to_value, conversion_value;
     String[] currency = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
@@ -64,6 +72,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+        editTextFrom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                from_dialog = new Dialog(MainActivity.this);
+                from_dialog.setContentView(R.layout.from_spinner);
+                from_dialog.getWindow().setLayout(720, 1000);
+                from_dialog.show();
+
+                EditText edittext = from_dialog.findViewById(R.id.edit_text);
+                ListView listview = from_dialog.findViewById(R.id.list_view);
+
+                ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, arraylist);
+                listview.setAdapter(adapter);
+
+                edittext.addTextChangedListener(new TextWatcher() {
+                    @Override
+                    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                    }
+
+                    @Override
+                    public void onTextChanged(CharSequence s, int start, int before, int count) {
+                        adapter.getFilter().filter(s);
+                    }
+
+                    @Override
+                    public void afterTextChanged(Editable s) {
+
+                    }
+                });
+                listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        editTextFrom.setText(adapter.getItem(position));
+                        from_dialog.dismiss();
+                        convert_from_value = adapter.getItem(position);
+                    }
+                });
+            }
+        });
 
 
 
