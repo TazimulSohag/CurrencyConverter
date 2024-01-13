b11f03514c902eac1ef172eefedddfd0b82d6031
Tazim <tazimulsohag@gmail.com>
2024-01-13 20:58:00 +0600

    base complete

--- app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
+++ app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
@@ -32,33 +32,6 @@ import java.math.RoundingMode;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    String convert_from_value, convert_to_value, conversion_value;
-    String[] currency = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN",
-            "BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL", "BSD", "BTC", "BTN", "BWP", "BYN", "BYR", "BZD",
-            "CAD", "CDF", "CHF", "CLF", "CLP", "CNY", "COP", "CRC", "CUC", "CUP", "CVE", "CZK",
-            "DJF", "DKK", "DOP", "DZD",
-            "EGP", "ERN", "ETB", "EUR",
-            "FJD", "FKP",
-            "GBP", "GEL", "GGP", "GHS", "GIP", "GMD", "GNF", "GTQ", "GYD",
-            "HKD", "HNL", "HRK", "HTG", "HUF",
-            "IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK",
-            "JEP", "JMD", "JOD", "JPY",
-            "KES", "KGS", "KHR", "KMF", "KPW", "KRW", "KWD", "KYD", "KZT",
-            "LAK", "LBP", "LKR", "LRD", "LSL", "LVL", "LYD",
-            "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRO", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN",
-            "NAD", "NGN", "NIO", "NOK", "NPR", "NZD",
-            "OMR",
-            "PAB", "PEN", "PGK", "PHP", "PKR", "PLN", "PYG",
-            "QAR",
-            "RON", "RSD", "RUB", "RWF",
-            "SAR", "SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLL", "SOS", "SRD", "STD", "SVC", "SYP", "SZL",
-            "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TWD", "TZS",
-            "UAH", "UGX", "USD", "UYU", "UZS",
-            "VEF", "VND", "VUV",
-            "WST",
-            "XAF", "XAG", "XCD", "XDR", "XOF", "XPF",
-            "YER",
-            "ZAR", "ZMK", "ZMW", "ZWL"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,10 +40,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
-
-
-    
 
     public void Close_MainActivity() {
         MainActivity.this.finish();
