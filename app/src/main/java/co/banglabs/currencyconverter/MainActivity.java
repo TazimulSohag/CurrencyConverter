b3250c684b47ab2fdf6c1c6b1c0b29b883f45518
Tazim <tazimulsohag@gmail.com>
2024-01-14 22:18:45 +0600

    base layout complete

--- app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
+++ app/src/main/java/co/banglabs/currencyconverter/MainActivity.java
@@ -1,35 +1,8 @@
 package co.banglabs.currencyconverter;
 
-import static java.lang.Math.round;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-
-import android.app.Dialog;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
